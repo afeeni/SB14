2de1476b0086ded92bb32e68370b797bfa7a6a07
Afeeni Phillips <afeeniphillips@Afeenis-MacBook-Pro-3.local>
2017-08-18 09:48:41 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller
+public class HomeController {
+    @Autowired
+    DirectorRepository directorRepository;
+
+    @RequestMapping("/")
+    public String index(Model model){
+        // 1st create a director
+        Director director = new Director();
+        director.setName("Stephen Bullock");
+        director.setGenre("Sci Fi");
+
+        // create a movie
+        Movie movie = new Movie();
+        movie.setTitle("Star Movie");
+        movie.setYear(2017);
+        movie.setDescription("About stars...");
+
+        // add movie to empty list
+        Set<Movie> movies = new HashSet<Movie>();
+        movies.add(movie);
+
+
+        movie = new Movie();
+        movie.setTitle("Deathstar Ewoks");
+        movie.setYear(2011);
+        movie.setDescription("About Ewoks on the Deathstar...");
+
+
+        // add list of movies to directors movie list
+        director.setMovies(movies);
+
+        // save directory to the database
+        directorRepository.save(director);
+
+        // Grad all directors from database and send them to the temple
+        model.addAttribute("directors", directorRepository.findAll());
+        return "index";
+
+    }
+
+
+
+
+
+}
